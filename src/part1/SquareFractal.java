3cec65c5489b42f56accfcdf700dbd13c89faa5d
Michelle Mao <maom@s.dcsdk12.org>
2016-03-29 09:53:00 -0600

    finished pic 3

--- src/part1/SquareFractal.java
+++ src/part1/SquareFractal.java
@@ -36,12 +36,12 @@ public class SquareFractal {
 		Color nextColor = new Color(red, green, blue);
 fractal(bottomLeft,  n-1, nextColor);
 		
-		
+fractal(topLeft,     n-1, nextColor);
+fractal(bottomRight, n -1, nextColor);
 		fractal(topRight,    n-1, nextColor);
 		
 		square.draw(color);
-		fractal(topLeft,     n-1, nextColor);
-		fractal(bottomRight, n -1, nextColor);
+		
 		
 		
 		
