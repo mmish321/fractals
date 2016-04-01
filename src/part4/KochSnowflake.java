cfb58938714eaf63d5f4e3530b19e38c72a50e41
Michelle Mao <maom@s.dcsdk12.org>
2016-04-01 10:19:54 -0600

    Started the curve

--- src/part4/KochSnowflake.java
+++ src/part4/KochSnowflake.java
@@ -1,10 +1,20 @@
 package part4;
 import java.awt.geom.Point2D;
 import draw.StdDraw;
+import part3.HTree;
 
 
 public class KochSnowflake {
 
-	// TODO: implement
+	public static void main(String[] args) {
+		KochSnowflake help = new KochSnowflake();
+		
+	}
+	public KochSnowFlake() {
+		StdDraw.setXscale(0, 150);
+		StdDraw.setYscale(0, 150);
+		StdDraw.clear(StdDraw.RED);
+	}
+ 
 
 }
